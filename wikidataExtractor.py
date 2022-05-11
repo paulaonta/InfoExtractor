45a2eb76ab84d08ba8af6ba1f11a553349aff144
Paula Ontalvilla <paulaonta@gmail.com>
2022-05-11 23:40:40 +0200

    get subclass of and send queries

--- wikidataExtractor.py
+++ wikidataExtractor.py
@@ -1,19 +1,17 @@
-from SPARQLWrapper import SPARQLWrapper, JSON, N3, XML,CSV
+from SPARQLWrapper import SPARQLWrapper, JSON, N3, XML, CSV
 import os
 import numpy
 import csv
 from qwikidata.sparql import (get_subclasses_of_item,
                               return_sparql_query_results)
 
-languages = ['en', 'es', 'eu', 'ca', 'fr']
-wdt = [ 'P31', 'P279']
-array1, array2 = [],[]
+languages = ['en']#, 'es', 'eu', 'ca', 'fr']
 
 def convertDictToArray(res):
     select_term = ""
     i = 0
     first_row = []
-    array  = []
+    array = []
     for result in res["head"]["vars"]:
         select_term = result
         array.insert(i, [])
@@ -30,6 +28,7 @@ def convertDictToArray(res):
 
     return first_row, array
 
+
 def getUniqueValuesIndices(array):
     # get unique values of diseases
     uniqueValues, indicesLists = numpy.unique(array, return_index=True)
@@ -43,8 +42,10 @@ def getUniqueValuesIndices(array):
 
     return z, lista, indicesList
 
+
 def getOtherValues(first_row, indicesList, array, lista):
     for j in range(1, len(first_row)):
+        i = -1
         for i in range(0, len(indicesList) - 1):
             l = array[j][indicesList[i]:indicesList[i + 1]]
             lagUV = numpy.unique(l)
@@ -59,80 +60,71 @@ def getOtherValues(first_row, indicesList, array, lista):
 
     return lista
 
+sparql = SPARQLWrapper("https://query.wikidata.org/")
+try:
+    sparql_query = ''' SELECT ?disease  
+                 WHERE{
+                  ?disease wdt:P31 wd:Q12136.
+                  }'''
+    res = return_sparql_query_results(sparql_query)
+    first_row, array = convertDictToArray(res)
+except:
+    print("error")
+
+wdt = [elem.split("http://www.wikidata.org/entity/")[1] for elem in array[0]]
 
 for lang in languages:
-    #create csv file for each language
+    first = True
+    # create csv file for each language
     folder = 'diseases_info_' + lang + '.csv'
     mydirname = './' + folder
     if not os.path.exists(mydirname):
         os.makedirs(os.path.dirname(mydirname), exist_ok=True)
 
-    sparql = SPARQLWrapper("https://query.wikidata.org/")
+    # open the csv file
+    myFile = open(mydirname, 'w')
+    writer = csv.writer(myFile)
 
-    try:
-        sparql_query = ''' SELECT DISTINCT ?diseaseLabel ?symptomsLabel ?treatmentLabel ?differentFromLabel ?riskLabel 
-         WHERE {
-            ?disease wdt:P31 ?m. 
-            ?m wdt:P279 wd:Q12136.
-          OPTIONAL { ?disease wdt:P780 ?symptoms. }
-          OPTIONAL {?disease wdt:P2176 ?treatment.}
-          OPTIONAL {?disease wdt:P1889 ?differentFrom.}
-          OPTIONAL { ?disease wdt:P5642 ?risk.}
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "''' + lang + '''". }
-        }
-        ORDER BY (?disease)'''
-        res = return_sparql_query_results(sparql_query)
-        first_row, array = convertDictToArray(res)
+    for prop in wdt:
+        try:
+            sparql_query = ''' SELECT DISTINCT ?diseaseLabel ?symptomsLabel ?treatmentLabel ?differentFromLabel ?riskLabel
+             ?causeLabel ?diagnosisLabel ?icd9 ?icd10 ?umls ?mesh 
+             WHERE {
+                ?disease wdt:P31 wd:''' + prop + '''.
+         
+              OPTIONAL { ?disease wdt:P923 ?diagnosis. }
+              OPTIONAL { ?disease wdt:P1692 ?icd9. }
+              OPTIONAL { ?disease wdt:P4229 ?icd10. }
+              OPTIONAL { ?disease wdt:P2892 ?umls. }
+              OPTIONAL { ?disease wdt:P486 ?mesh. }
+              SERVICE wikibase:label { bd:serviceParam wikibase:language "''' + lang + '''". }
+            }
+            ORDER BY (?disease)'''
 
-        sparql_query2 = ''' SELECT DISTINCT ?diseaseLabel ?causeLabel ?diagnosisLabel ?icd9 ?icd10 ?umls ?mesh 
-         WHERE {
-            ?disease wdt:P31 ?m. 
-            ?m wdt:P279 wd:Q12136.
-          OPTIONAL { ?disease wdt:P828 ?cause. }
-          OPTIONAL { ?disease wdt:P923 ?diagnosis. }
-          OPTIONAL { ?disease wdt:P1692 ?icd9. }
-          OPTIONAL { ?disease wdt:P4229 ?icd10. }
-          OPTIONAL { ?disease wdt:P2892 ?umls. }
-          OPTIONAL { ?disease wdt:P486 ?mesh. }
-          SERVICE wikibase:label { bd:serviceParam wikibase:language "''' + lang + '''". }
-        }
-        ORDER BY (?disease)'''
-        res2 = return_sparql_query_results(sparql_query2)
-        first_row2, array2 = convertDictToArray(res2)
+            res = return_sparql_query_results(sparql_query)
+            first_row, array = convertDictToArray(res)
+        except:
+            print("error")
+            pass
+        if array[0]:
+            # get diseases values
+            uniqueValues, lista, indicesList = getUniqueValuesIndices(array[0])
+            # get other values
+            lista = getOtherValues(first_row, indicesList, array, lista)
 
-        #get diseases values
-        uniqueValues, lista, indicesList = getUniqueValuesIndices(array[0])
-        uniqueValues2, lista2, indicesList2 = getUniqueValuesIndices(array2[0])
-
-        #get other values
-        lista = getOtherValues(first_row, indicesList, array, lista)
-        lista2 = getOtherValues(first_row2, indicesList2, array2, lista2)
-
-        # open the csv file
-        myFile = open(mydirname, 'w')
-        writer = csv.writer(myFile)
-
-        # inset values in the csv file. We need to merge values
-        # first, insert the first row
-        for i in range(1, len(first_row)): #merge first_row and first_row2
-            first_row2.append(first_row[i])
-
-        #insert in the csv
-        writer.writerow(first_row2)
-
-        #insert the others values
-        #we have two list to merge: lista and lista2
-        j = 0
-        for elem in uniqueValues2:
-            if elem in uniqueValues:#append the values of the new columns
-                pos = uniqueValues.index(elem)
-                for i in range(1, len(first_row)):
-                    lista2[j].append(lista[pos][i])
+            if first:
+                # insert in the csv
+                writer.writerow(first_row)
+                first = False
+                uniqueValues2 = uniqueValues.copy()
+                lista2 = lista.copy()
             else:
-                lista2[j].append(" ")
-            j += 1
+                # insert the others values
+                # we have two list to merge: lista and lista2
+                for elem in uniqueValues:
+                    if elem in uniqueValues2:  # append the values
+                        pos = uniqueValues.index(elem)
+                        lista2.append(lista[pos])
 
-        writer.writerows(lista2)
+    writer.writerows(lista2)
 
-    except:
-        print("error")
