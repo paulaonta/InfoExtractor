0c50c43c3091ffaf23113649caf1e40ae453bc4a
Paula Ontalvilla <paulaonta@gmail.com>
2022-06-15 18:06:08 +0200

    all the diseases filtered in english

--- wikidataExtractor.py
+++ wikidataExtractor.py
@@ -60,18 +60,71 @@ def getOtherValues(first_row, indicesList, array, lista):
 
     return lista
 
+def getValues(first_row, array):
+    lista = []
+    lista.append(array[0][0])
+    for j in range(1, len(first_row)):
+        aux = numpy.unique(array[j])
+        string = ",".join(aux)
+        lista.append(string.replace(';', ','))
+    return lista
+
 sparql = SPARQLWrapper("https://query.wikidata.org/")
+
+# create file for log
+file = 'errors_log'
+errorFile = './' + file
+if not os.path.exists(errorFile):
+    os.makedirs(os.path.dirname(errorFile), exist_ok=True)
+
+
 try:
-    sparql_query = ''' SELECT ?disease  
-                 WHERE{
-                  ?disease wdt:P31 wd:Q12136.
-                  }'''
+    sparql_query = ''' SELECT ?item2 ?item2Label
+        WHERE
+        {
+        ?item (wdt:P279*) wd:Q12136. # subclass of
+        ?item2 (wdt:P31) ?item. # instance of
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
+        }'''
     res = return_sparql_query_results(sparql_query)
-    first_row, array = convertDictToArray(res)
+    first_row, prop = convertDictToArray(res)
+
+    sparql_query = ''' SELECT ?item2 ?itemLabel 
+        WHERE
+        {
+       { ?item (wdt:P279*) wd:Q9190427. }# subclass of animals diseases}
+        UNION
+        { ?item (wdt:P279*) wd:Q2662845.} #plants diseases
+           UNION
+        { ?item (wdt:P279*) wd:Q98379923.} #aspect in a geographic region
+           UNION
+        { ?item (wdt:P279*) wd:Q216866.} #siames
+          UNION
+        { ?item (wdt:P279*) wd:Q44512.} #epidemic
+          UNION
+        { ?item (wdt:P279*) wd:Q178059.} #paraphilia
+         UNION
+        { ?item (wdt:P279*) wd:Q191355.}  #hunger strike
+          UNION
+        { ?item (wdt:P279*) wd:Q4215775.} #metal poisoning
+          UNION
+        { ?item (wdt:P279*) wd:Q114953.} #poisoning
+          
+        ?item2 (wdt:P31) ?item. # instance of
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". } # Helps get the label in your language, if not, then en language
+        }'''
+    res = return_sparql_query_results(sparql_query)
+    first_row, prop1 = convertDictToArray(res)
+
+    #remove the elements in array1 that there are in array
+    for elem in prop1[0]:
+        if elem in prop[0]:
+            prop[0].remove(elem)
+
+    wdt = [elem.split("http://www.wikidata.org/entity/")[1] for elem in prop[0]]
+
 except:
     print("error")
-
-wdt = [elem.split("http://www.wikidata.org/entity/")[1] for elem in array[0]]
 
 for lang in languages:
     first = True
@@ -84,14 +137,20 @@ for lang in languages:
     # open the csv file
     myFile = open(mydirname, 'w')
     writer = csv.writer(myFile)
-
-    for prop in wdt:
+    i, errorCount = 0, 0
+    lista2 = []
+    while i < len(wdt):
+        prop_num = wdt[i]
         try:
             sparql_query = ''' SELECT DISTINCT ?diseaseLabel ?symptomsLabel ?treatmentLabel ?differentFromLabel ?riskLabel
              ?causeLabel ?diagnosisLabel ?icd9 ?icd10 ?umls ?mesh 
              WHERE {
-                ?disease wdt:P31 wd:''' + prop + '''.
-         
+                ?disease wdt:* wd:''' + prop_num + '''.
+              OPTIONAL { ?disease wdt:P780 ?symptoms. }
+              OPTIONAL { ?disease wdt:P2176 ?treatment. }
+              OPTIONAL { ?disease wdt:P1889 ?differentFrom. }
+              OPTIONAL { ?disease wdt:P5642 ?risk. }
+              OPTIONAL { ?disease wdt:P828 ?cause. }
               OPTIONAL { ?disease wdt:P923 ?diagnosis. }
               OPTIONAL { ?disease wdt:P1692 ?icd9. }
               OPTIONAL { ?disease wdt:P4229 ?icd10. }
@@ -99,32 +158,27 @@ for lang in languages:
               OPTIONAL { ?disease wdt:P486 ?mesh. }
               SERVICE wikibase:label { bd:serviceParam wikibase:language "''' + lang + '''". }
             }
-            ORDER BY (?disease)'''
-
+            '''
             res = return_sparql_query_results(sparql_query)
             first_row, array = convertDictToArray(res)
+            if first:
+                lista2.append(first_row)
+                first = False
+            errorCount = 0
+            i += 1
+            if array[0]:  # if it is not empty
+                lista = getValues(first_row, array)
+                lista2.append(lista)
         except:
+            errorCount += 1
+            if errorCount == 5:
+                #append in the logger
+                myFile = open(errorFile, 'a')
+                myFile.write("This disease: \"" +prop[1][i] + "\"" + " (" + prop_num + ") can't be load\n")
+                errorCount = 0
+                i += 1
             print("error")
             pass
-        if array[0]:
-            # get diseases values
-            uniqueValues, lista, indicesList = getUniqueValuesIndices(array[0])
-            # get other values
-            lista = getOtherValues(first_row, indicesList, array, lista)
-
-            if first:
-                # insert in the csv
-                writer.writerow(first_row)
-                first = False
-                uniqueValues2 = uniqueValues.copy()
-                lista2 = lista.copy()
-            else:
-                # insert the others values
-                # we have two list to merge: lista and lista2
-                for elem in uniqueValues:
-                    if elem in uniqueValues2:  # append the values
-                        pos = uniqueValues.index(elem)
-                        lista2.append(lista[pos])
 
     writer.writerows(lista2)
 
